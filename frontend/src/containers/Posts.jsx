7aae05de89c8b7ed8b95583cdb358a4a767cc2f5
rioto.nagano <kingdom950927@gmail.com>
2022-12-04 12:01:15 +0900

    PostsコンポーネントからAPIを叩く

--- frontend/src/containers/Posts.jsx
+++ frontend/src/containers/Posts.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+// apis
+import { fetchPosts } from '../apis/posts';
 
 export const Posts = () => {
+
+   useEffect(() => {
+    fetchPosts()
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
+
   return (
     <>
       Post一覧
