691663e486c7362a0881c14057f7ba9069f959ed
rioto.nagano <kingdom950927@gmail.com>
2022-12-04 16:19:42 +0900

    インデントの修正

--- frontend/src/apis/posts.js
+++ frontend/src/apis/posts.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { postsIndex } from '../urls/index'
 
-export const fetchPosts =() => {
+export const fetchPosts = () => {
   return axios.get(postsIndex)
   .then(res => {
     return res.data
