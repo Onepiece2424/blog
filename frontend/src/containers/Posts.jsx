9f141da4a8c2311eb7eba6e959bedb361869c202
rioto.nagano <kingdom950927@gmail.com>
2022-12-06 21:08:12 +0900

    postを配列として取得（表示はまだ）

--- frontend/src/containers/Posts.jsx
+++ frontend/src/containers/Posts.jsx
@@ -1,7 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useReducer } from 'react';
 
 // apis
 import { fetchPosts } from '../apis/posts';
+
+// reducers
+import {
+  initialState,
+  postsActionTyps,
+  postsReducer,
+} from '../reducers/posts';
 
 export const Posts = () => {
 
@@ -11,6 +18,26 @@ export const Posts = () => {
       console.log(data)
     )
   }, [])
+
+  const [state, dispatch] = useReducer(postsReducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: postsActionTyps.FETCHING });
+    fetchPosts()
+    .then((data) =>
+      dispatch({
+        type: postsActionTyps.FETCH_SUCCESS,
+        payload: {
+          posts: data.data
+        }
+      })
+    )
+  }, [])
+
+  // postsを配列として取得
+  console.log(state.postsList.posts)
+  const array = state.postsList.posts
+  console.log(array)
 
   return (
     <>
