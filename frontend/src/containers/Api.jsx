b5e988113ec463f9ba396df041cf91c39eb02189
rioto.nagano <kingdom950927@gmail.com>
2022-12-08 21:23:36 +0900

    axios.postででーたのデータの生成

--- frontend/src/containers/Api.jsx
+++ frontend/src/containers/Api.jsx
@@ -18,13 +18,37 @@ const Api = (() => {
   console.log(users)
 
 
-  
+  // axios.postでデータの生成
+  const [name, setName] = useState('')
+
+  const handleChange = (e) => {
+  setName(e.target.value)
+}
+
+const createNewUser = () => {
+  axios.post('https://jsonplaceholder.typicode.com/users', {
+    name: name
+  })
+  .then(response => {
+    setUsers([...users, response.data])
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}
 
   return (
     <>
       {users.map((user, index) => (
         <div key={index}>{user.name}</div>
       ))}
+      <input value={name} onChange={handleChange} /><br />
+      <button onClick={createNewUser}>作成</button>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>{user.name}</li>
+        ))}
+      </ul>
     </>
   )
 })
