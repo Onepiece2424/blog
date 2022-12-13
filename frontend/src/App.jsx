76cf2cbb3da739e2aae33c0a0028fcf66d7ef562
rioto.nagano <kingdom950927@gmail.com>
2022-12-14 05:58:54 +0900

    warningの解決

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import './App.css';
-
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 // components
 // import Reference from './containers/Reference';
 import Contact from './containers/contact/Contact';
 // import Edamam from './containers/api/Edamam'
 import HomeTown from './containers/home/HomeTown'
+import Count from './containers/Count'
 
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+
+
 
 function App() {
   const REACT_APP_S = "6LeHHG4jAAAAAKLzo5sZo5PbUJsXPIyHBC7G0yw5"
 
   return (
     <GoogleReCaptchaProvider reCaptchaKey={REACT_APP_S} language="ja">
+      <Count />
       <Contact />
-      {/* <Edamam /> */}
       <br></br>
       <br></br>
       <HomeTown />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
@@ -7,9 +7,11 @@ import App from './App';
 import { Provider } from "react-redux"
 import store from "./store/index";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
