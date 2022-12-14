474970338087d8f5c9eb9844ba673a0008079253
rioto.nagano <kingdom950927@gmail.com>
2022-12-14 21:28:03 +0900

    redux-devtoolsの導入

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -30,7 +30,10 @@ const rootReducer = combineReducers({
   form: reduxFormReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 console.log(store.getState());
 
 export default store;
