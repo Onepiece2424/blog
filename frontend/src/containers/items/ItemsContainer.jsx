f41b7bea201518da58ceeeb7dcdd7e1db5a1cec5
rioto.nagano <kingdom950927@gmail.com>
2022-12-20 05:52:05 +0900

    useSelectorにより, initialSttaeの取得

--- frontend/src/containers/items/ItemsContainer.jsx
+++ frontend/src/containers/items/ItemsContainer.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import {initialState} from '../../reducers/addtodo/addtodo'
+import { useSelector } from "react-redux";
+
 
 const ItemsContainer = (props) => {
 
   const {itemdata} = props
+  const item_name = useSelector((state) => initialState.item_name);
+  console.log(item_name)
 
   return (
     <>
       <h3>登録した商品一覧</h3>
+      <>{item_name}</>
       <>商品：{initialState.item_name}</>
       <br></br>
       <>数量：{initialState.quantity}</>
