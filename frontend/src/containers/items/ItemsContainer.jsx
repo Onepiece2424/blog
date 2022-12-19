680f202241d572619c3c638f6cab9f4fe0ae5d53
rioto.nagano <kingdom950927@gmail.com>
2022-12-19 22:01:01 +0900

    initialStateからデータ取得し表示

--- frontend/src/containers/items/ItemsContainer.jsx
+++ frontend/src/containers/items/ItemsContainer.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import store from '../../store';
 
 const ItemsContainer = (props) => {
 
   const {itemdata} = props
 
+  console.log(store.getState().addtodoReducer)
+
   return (
     <>
       <h3>登録した商品一覧</h3>
+      <>{store.getState().addtodoReducer.item_name}</>
+      <>{store.getState().addtodoReducer.quantity}</>
       <>
         {itemdata.map((item) =>
           <div key={item.id}>
