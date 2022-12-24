c83ca33a4c0422e21b734d83c3438417d43683e1
rioto.nagano <kingdom950927@gmail.com>
2022-12-25 08:00:08 +0900

    不要な部分の削除

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,12 @@ import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 // components
 // import Reference from './containers/Reference';
-import Contact from './containers/contact/Contact';
+// import Contact from './containers/contact/Contact';
 // import Edamam from './containers/edamam_api/Edamam'
 // import HomeTown from './containers/home/HomeTown'
-import Count from './containers/count/Count'
-import MainForm from './containers/form/MainForm';
-import Pointer from './containers/point/Pointer';
+// import Count from './containers/count/Count'
+// import MainForm from './containers/form/MainForm';
+// import Pointer from './containers/point/Pointer';
 // import AlphaNumeral from './containers/jksonplaceholder_api/AlphaNumeral';
 import MainContainer from './containers/items/MainContainer';
 
@@ -17,12 +17,12 @@ function App() {
   return (
     <GoogleReCaptchaProvider reCaptchaKey={process.env.REACT_APP_S} language="ja">
       <MainContainer />
-      <Contact />
+      {/* <Contact /> */}
       <br></br>
       <br></br>
-      <Pointer />
+      {/* <Pointer />
       <MainForm />
-      <Count />
+      <Count /> */}
       {/* <HomeTown />
       <Reference />
       <br></br>
