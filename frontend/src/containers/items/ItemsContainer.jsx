e9a6efa7e38a964b3060220e4886ae287076eba9
rioto.nagano <kingdom950927@gmail.com>
2022-12-25 15:08:38 +0900

    不要な部分の削除

--- frontend/src/containers/items/ItemsContainer.jsx
+++ frontend/src/containers/items/ItemsContainer.jsx
@@ -1,8 +1,8 @@
-import React,{useEffect} from 'react'
+import React from 'react'
 import {initialState} from '../../reducers/addtodo/addtodo'
 import { useSelector } from "react-redux";
-import { callback,call } from '../func/calback';
-import { test_promise } from '../func/promise';
+// import { callback,call } from '../func/calback';
+// import { test_promise } from '../func/promise';
 
 
 const ItemsContainer = (props) => {
@@ -10,21 +10,21 @@ const ItemsContainer = (props) => {
   const {itemdata} = props
   const initial_item_name = useSelector((state) => initialState.item_name);
   const initial_quantity = useSelector((state) => initialState.quantity);
-  console.log(initial_item_name, initial_quantity)
+  // console.log(initial_item_name, initial_quantity)
 
-  useEffect(() => {
-    console.log("1番目");
-    new Promise((resolve) => {
-      setTimeout(() => {
-        console.log("2番目(1秒後に実行)");
-        resolve();
-      }, 1000);
-    }).then(() => {
-      console.log("3番目");
-    });
-    call(callback())
-    test_promise(true)
-  },[])
+  // useEffect(() => {
+  //   console.log("1番目");
+  //   new Promise((resolve) => {
+  //     setTimeout(() => {
+  //       console.log("2番目(1秒後に実行)");
+  //       resolve();
+  //     }, 1000);
+  //   }).then(() => {
+  //     console.log("3番目");
+  //   });
+  //   call(callback())
+  //   // test_promise(true)
+  // },[])
 
   return (
     <>
