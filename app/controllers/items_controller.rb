ddfb7b9066ca274c558d3d5cedde4a8de23c4fbe
rioto.nagano <kingdom950927@gmail.com>
2022-12-25 18:04:32 +0900

    商品登録をscore判定に依存させる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,8 +8,13 @@ class ItemsController < ApplicationController
     token = params[:token]
     recaptcha_action = params[:action]
     verify_recaptcha_response = verify_recaptcha?(token, recaptcha_action)
-    @item = Item.create(item_params)
-    render json: @item
+    # render json: verify_recaptcha_response
+    unless verify_recaptcha_response
+      render json: verify_recaptcha_response
+    else
+      @item = Item.create(item_params)
+      render json: verify_recaptcha_response
+    end
   end
 
   def update
--- frontend/src/containers/items/FormContainer.jsx
+++ frontend/src/containers/items/FormContainer.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useRef } from 'react';
 import axios from 'axios'
 import { reduxForm, Field } from 'redux-form';
 // import showResults from '../func/showResults';
@@ -8,8 +8,8 @@ import {useGoogleReCaptcha} from 'react-google-recaptcha-v3';
 
 const FormContainer = (props) => {
 
-  const { handleSubmit } = props;
-
+  const { handleSubmit, change } = props;
+  const elm = useRef(null)
   const [name, setName] = useState('')
   const [quantity, setQuantity] = useState('')
 
@@ -26,17 +26,19 @@ const FormContainer = (props) => {
   const createItem = async() => {
 
     const token = await executeRecaptcha('create');
-    console.log(token)
+    elm.current = change("recaptchatoken", token)
+    console.log(elm.current)
 
     await axios.post('http://localhost:3000/items',
     { item_name: name,
       quantity: quantity,
+      token: token
     })
     .then((response) => {
-      console.log(response);
+      console.log(response, '商品登録完了です。');
     })
     .catch((error) =>{
-      console.log(error)
+      console.log(error, '商品登録に失敗しました。')
     })
   };
 
@@ -49,7 +51,7 @@ const FormContainer = (props) => {
         <Field name='数量' component='input' value={quantity} onChange={quantityChange} placeholder='数量' />
         <br></br>
         <br></br>
-        <Field name="recaptchatoken" component="input" type="hidden" />
+        <Field name="recaptchatoken" component="input" type="hidden" value={elm} />
         <button type="submit" onClick={createItem}>商品登録</button>
       </form>
     </>
